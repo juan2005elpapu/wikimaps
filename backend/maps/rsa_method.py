b23a7323a9618426f405c59e1ed68b9d051bc536
Alexander Herrera <147080641+A13XAnDer01@users.noreply.github.com>
2025-03-06 08:28:00 -0500

    Update rsa_method.py

--- backend/maps/rsa_method.py
+++ backend/maps/rsa_method.py
@@ -1,45 +1,36 @@
 class Encrypter: 
 
     def __init__(self, password): 
-
-        self.modulus = 2491
-        self.publicExponent = 37 
-        self.privateExponent = 937 
+        self.modulus = 3233
+        self.publicExponent = 65537
+        self.privateExponent = 2753  
         self.password = password
 
-    def From_String_To_RSA(self):
-
+    def RSA_Encrypt(self): 
         listOfNum=[]
         for letter in self.password: 
             letter = int.from_bytes(letter.encode(), 'big')
             listOfNum.append(letter)
-        return listOfNum
-    
-    def RSA_Encrypt(self): 
 
-        listOfNum = self.From_String_To_RSA()
         final_Password = ''
         for Nums in listOfNum: 
             final_Num = (Nums**self.publicExponent) % self.modulus
-            final_Password = final_Password+str(hex(final_Num))+' '
-        return final_Password
+            final_Password += str(hex(final_Num))+' '
+        return final_Password.rstrip()
     
-    def RSA_dencrypt(self):
+    def RSA_Decrypt(self, encripted): 
+        encrypted_nums = encripted
+        encrypted_nums = encrypted_nums.split(' ')
 
-        final_Password = self.RSA_Encrypt()
-        listOfHeX = final_Password.strip().split(' ')
-        listOfNum = []
-        listOfFinal = []
-        for numsHex in listOfHeX: 
-            numDec = int(str(numsHex), 16)
-            final_Num = pow(numDec, self.privateExponent)
-            listOfNum.append(numDec)
-            listOfFinal.append(final_Num)
-        print(listOfNum)
-        return listOfNum
+        decrypted_message = ''
+        for encrypted_num_str in encrypted_nums:
+            encrypted_num = int(encrypted_num_str, 16)
+            decrypted_num = (encrypted_num ** self.privateExponent) % self.modulus
+            decrypted_message += chr(decrypted_num)
 
+        return decrypted_message
+    
 
 encriptador = Encrypter('hola')
-print(encriptador.From_String_To_RSA())
 print(encriptador.RSA_Encrypt())
-print(encriptador.RSA_dencrypt())
+print(encriptador.RSA_Decrypt(encriptador.RSA_Encrypt()))
